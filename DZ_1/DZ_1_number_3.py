2298788101364aade109fce8760ed52c81fe9461
Deniska400 <karpenkodenis400@gmail.com>
2024-09-17 12:09:45 +0300

    Update

--- DZ_1/DZ_1_number_3.py
+++ DZ_1/DZ_1_number_3.py
@@ -1,5 +1,8 @@
+print('Введите длину плитки шоколада:')
 length = int(input())
+print('Введите ширину плитки шоколада:')
 width = int(input())
+print('Введите размер куска:')
 size = int(input())
 
 if size <= length * width and (size % length == 0 or size % width == 0):
--- DZ_1_number_1.py
+++
@@ -1,8 +0,0 @@
-print('Введите пятизначное число')
-number = input()
-
-while len(number) != 5:
-    print('Введите пятизначное число')
-    number = input()
-
-print(number[0], number[3:0:-1], number[4], sep='')
