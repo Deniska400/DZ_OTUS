9cc479ee1e45c941ff994948fb82e47a8412a34c
Deniska400 <karpenkodenis400@gmail.com>
2024-10-04 08:39:24 +0300

    DZ_3

--- DZ_3/DZ_3_number_4.py
+++ DZ_3/DZ_3_number_4.py
@@ -15,6 +15,35 @@ def check_string(value: str):
             print('Введите значение повторно')
             value = input()
 
+def check_age(value):
+    while True:
+        if 17 < int(value) < 61:
+            return value
+            break
+        else:
+            print('Введите корректный возраст')
+            value = input()
+
+def up_to_8(value):
+    if len(value) < 8:
+        value = '0' * (8 - len(value)) + str(value)
+        return value
+
+def check_id(value):
+    while value in storage:
+        print('Этот id занят, введите другой')
+        value = input()
+        if len(value) < 8:
+            value = '0' * (8 - len(value)) + str(value)
+    return value
+
+def print_dict():
+    for key in sorted(storage):
+        print(f'{key}:', end=' ')
+        for i in sorted(storage[key].items()):
+            print(' '.join(i))
+
+storage = {}
 
 while True:
     print('Введите имя:')
@@ -31,6 +60,8 @@ while True:
         break
     print('Введите возраст:')
     age = input()
+    age = check_age(age)
+
     if stop(age):
         print('Программа завершена')
         break
@@ -39,4 +70,9 @@ while True:
     if stop(id_people):
         print('Программа завершена')
         break
-print(name, surname)
+    id_people = up_to_8(id_people)
+    id_people = check_id(id_people)
+
+    storage[id_people][name] = storage.setdefault(id_people, {}).setdefault(name, '') + str(surname) + ' ' + str(age)
+
+print_dict()
--- DZ_3/Test.py
+++
@@ -1,2 +0,0 @@
-a = 'gHHHHH'
-a.capitalize()
